9a37fc8ab3a1b1d42d885f661a52c2fdff3fce34
RautRA <168980384+RautRA@users.noreply.github.com>
2024-06-09 19:46:17 +0530

    convert the code to get user's choice with gui buttons

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+const choiceBtns = document.querySelectorAll(".choice-btn");
+const gameResult = document.querySelector("#game-result");
+
 function getComputerChoice() {
     let computerChoice;
 
@@ -15,72 +20,35 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt("Rock, Paper or Scissors? What's your choice?");
+function playRound(e) {
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
 
-    if(playerChoice === null) {
-        return null;
-    }
-
-    playerChoice = playerChoice.trim().toLowerCase();
-
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return playerChoice;
+    if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock.");
+        computerScore++;
+    } else if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win! Rock beats Scissors.");
+        playerScore++;
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper.");
+        computerScore++;
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("You win! Paper beats Rock.");
+        playerScore++;
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors.");
+        computerScore++;
+    }   else if (playerChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win! Scissors beats Paper.");
+        playerScore++;
+    } else if ( playerChoice === computerChoice) {
+        console.log("It's a tie.");
     } else {
-        console.log("Sorry you can only choose between Rock, Paper or Scissors");
-        
-        /* keep running the getHumanChoice function till a valid choice is 
-        provided or game is cancelled */
-        return getPlayerChoice();
+        console.log("Oh Uh! Wrong Choice. Something went wrong!");
     }
 }
 
-function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    let playerChoice;
-    let computerChoice;
-
-    function playRound() {
-        playerChoice = getPlayerChoice();
-        computerChoice = getComputerChoice();
-    
-        if (playerChoice === "rock" && computerChoice === "paper") {
-            console.log("You lose! Paper beats Rock.");
-            computerScore++;
-        } else if (playerChoice === "rock" && computerChoice === "scissors") {
-            console.log("You win! Rock beats Scissors.");
-            playerScore++;
-        } else if (playerChoice === "paper" && computerChoice === "scissors") {
-            console.log("You lose! Scissors beats Paper.");
-            computerScore++;
-        } else if (playerChoice === "paper" && computerChoice === "rock") {
-            console.log("You win! Paper beats Rock.");
-            playerScore++;
-        } else if (playerChoice === "scissors" && computerChoice === "rock") {
-            console.log("You lose! Rock beats Scissors.");
-            computerScore++;
-        }   else if (playerChoice === "scissors" && computerChoice === "paper") {
-            console.log("You win! Scissors beats Paper.");
-            playerScore++;
-        } else if ( playerChoice === computerChoice) {
-            console.log("It's a tie.");
-        } else {
-            console.log("You cancelled the game");
-            return;
-        }
-    }
-
-    for(let i = 0; i < 5; i++) {
-        if (playerChoice !== null) {
-            playRound();
-        } else {
-            break;
-        }
-    }
-
-    console.log(`Final Score is: You - ${playerScore}, Computer - ${computerScore}`);
-}
-
-playGame();
+choiceBtns.forEach(btn => {
+    btn.addEventListener("click", playRound);
+});
